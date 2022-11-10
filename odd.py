06da26787c85764ac0e4417ed8053356086a8825
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2022-11-10 20:50:00 +0200

    First question in the loop section

---
+++ odd.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Takes a number and prints all odd integers
+x = eval(input("Enter any mnumber: "))
+for i in range (x):
+    if i % 2 == 0 :
+        continue
+    print(i)
+
+ 
+print (x)
